9123d1c643326ad0b96f3b6022ad09fdaec91deb
RottenGreif <109813152+RottenGreif@users.noreply.github.com>
2023-01-23 09:32:46 -0600

    Promedio y guarda en archivo

--- 1_archivos/1_archivos.cpp
+++ 1_archivos/1_archivos.cpp
@@ -6,8 +6,25 @@
 #include <fstream>
 #define MAXSIZE 128
 
+void ejemploApuntadores()
+{
+    std::cout << "ejemplo de apuntadores\n";
+    int valor = 66;
+    int  *ref2 = &valor; //operador de referencia
+    *ref2 = 128;
+    printf("Valor: %i  Mem:%#010x \n", *ref2, ref2);
+    std::cout << "valor = " << valor << std::endl;
+}
+
+void acumular(float & a, float b)
+{
+    a += b;
+}
 int main()
 {
+    float valor = 0;
+    int num_vals = 0;
+
     //enviar la cadena HW al flujo std::cout
     std::cout << "Leyendo archivo" << std::endl;
 
@@ -24,8 +41,15 @@ int main()
         while ( !file.eof())
         {
             file.getline(buffer, MAXSIZE);
-            std::cout << buffer << std::endl;
+            acumular(valor, atof(buffer));
+
+            std::cout << "valor acumulado: " << valor << std::endl;
+            num_vals++;
         }
+        valor /= num_vals;
+
+        std::ofstream file_o("datos.txt", std::ofstream::app ); 
+        file_o << "\npromedio: " << valor << std::endl;
     }
     else
     {
